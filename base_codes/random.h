9beba9abe63c315425454c86b889168cc4d541e5
hjjonas <h.j.jonas@uva.nl>
2024-02-18 13:40:35 +0100

    added random.h

---
+++ base_codes/random.h
@@ -0,0 +1,25 @@
+
+// random.h
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+// extern int global_variable;
+
+/*---------------------------------------------------------------------------*/
+/*------------------GLOBAL FUNCTIONS------------------------------------*/
+
+extern double RandomNumber(void);
+extern void InitializeRandomNumberGenerator(double);
+extern double RandomGaussianNumber();
+extern vector RandomBrownianVector(double);
+extern vector RandomVector(double);
+extern double RandomVelocity(double);
+extern vector RandomUnitVector(void);
+extern int Random_urandom(void);
+extern vector RandomVector3D(vector ) ;
+extern double RandomNumberRange(double , double );
+extern int RandomIntegerRange(int  , int );
+extern vector RandomVector1(void) ;
+
+#endif
