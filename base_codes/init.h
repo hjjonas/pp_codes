f07516f53cabf518b348e8edf751ce62bf25ec78
hjjonas <h.j.jonas@uva.nl>
2024-02-18 13:24:40 +0100

    added gobal function: setup_simulation, commented local functions out

--- base_codes/init.h
+++ base_codes/init.h
@@ -5,28 +5,32 @@
 
 // extern int global_variable;
 
+/*---------------------------------------------------------------------------*/
+/*------------------GLOBAL FUNCTIONS------------------------------------*/
+
+extern void setup_simulation( void );
 
 /*---------------------------------------------------------------------------*/
 /*------------------LOCAL FUNCTIONS------------------------------------*/
 
-void check_input_with_MAXDEFS(void);
-vector check_read_unitvec(vector );
-double find_forcecutoff_distance(void );
-double find_minimum_of_potential(void);
-double find_trunc_of_Saccent_1(int);
-double find_zcut(double );
-double find_overlap_distance(void);
-void gravitational_parameters(void);
-void init_model(Slice *);
-void init_simtype(Slice *);
-void print_input(Slice *);
-void print_particle_properties(void);
-void ratcheting_prep(Slice *);
-int ratcheting_prep_conditions(int , int );
-void read_input(Slice *);
-void restart_read_time(Slice *);
-void setup_delta(void);
-void setup_mc_move(MC *);
+// void check_input_with_MAXDEFS(void);
+// vector check_read_unitvec(vector );
+// double find_forcecutoff_distance(void );
+// double find_minimum_of_potential(void);
+// double find_trunc_of_Saccent_1(int);
+// double find_zcut(double );
+// double find_overlap_distance(void);
+// void gravitational_parameters(void);
+// void init_model(Slice *);
+// void init_simtype(Slice *);
+// void print_input(Slice *);
+// void print_particle_properties(void);
+// void ratcheting_prep(Slice *);
+// int ratcheting_prep_conditions(int , int );
+// void read_input(Slice *);
+// void restart_read_time(Slice *);
+// void setup_delta(void);
+// void setup_mc_move(MC *);
 
 
 /*---------------------------------------------------------------------------*/
