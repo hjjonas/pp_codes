16f446d5d6063cdbce40247b467d918a66e3b2b7
hjjonas <h.j.jonas@uva.nl>
2024-02-18 21:21:38 +0100

    main.c: made all functions available

--- base_codes/main.c
+++ base_codes/main.c
@@ -2,8 +2,8 @@
 
 Slice        *slice, *psl_old, *start_slice, *copyslice;
 System       sys;
-
 vector        nulvec;
+
 
 
 // The main function of the simulation program
@@ -18,70 +18,70 @@ int main(int argc, char **argv) {
     // Call function to set up simulation parameters
     setup_simulation();
     
-    // // Plot the potential if specified in parameters
-    // plotpotential();
+    // Plot the potential if specified in parameters
+    plotpotential();
     
-    // // Check the random number generator if specified in parameters
-    // check_random();
+    // Check the random number generator if specified in parameters
+    check_random();
     
     // // Warm up the Monte Carlo system if specified in parameters
-    // if (sys.mc_warmup > 0) {
-    //     mc_warmup(&slice[0]);
-    // }
+    if (init_conf.mc_warmup > 0) {
+        mc_warmup(&slice[0]);
+    }
 
-    // // Outer loop for the number of cycles specified in parameters
-    // for (icycle = 0; icycle < sys.ncycle1; icycle++) {
+    // Outer loop for the number of cycles specified in parameters
+    for (int icycle = 0; icycle < sys.ncycle1; icycle++) {
         
-    //     // Print block number
-    //     printf("\nBLOCK %d\n", icycle);
+        // Print block number
+        printf("\nBLOCK %d\n", icycle);
         
-    //     // Inner loop for the number of cycles specified in parameters
-    //     for (jcycle = 0; jcycle < sys.ncycle2; jcycle++) {
+        // Inner loop for the number of cycles specified in parameters
+        for ( int jcycle = 0; jcycle < sys.ncycle2; jcycle++) {
             
-    //         // If reading path from file is specified in parameters
-    //         if (sys.read_path > 0) {
+            // If reading path from file is specified in parameters
+            if (init_conf.read_path > 0) {
                 
-    //             // Read coordinates from file
-    //             read_coordinates_from_file(&slice[0], sys.read_path*(icycle*sys.ncycle2+jcycle));
+                // Read coordinates from file
+                read_coordinates_from_file(&slice[0], init_conf.read_path*(icycle*sys.ncycle2+jcycle));
 
-    //         // If running Brownian dynamics simulation is specified in parameters
-    //         } else if (sys.sim_type == BMD_ALGORITHM) {
+            // If running Brownian dynamics simulation is specified in parameters
+            } else if (sys.sim_type == BMD_ALGORITHM) {
                 
-    //             // Run Brownian dynamics cycle
-    //             bmdcycle(&slice[0]);
+                // Run Brownian dynamics cycle
+                bmdcycle(&slice[0]);
                 
-    //             // If printing trajectory is specified in parameters
-    //             if (analysis.print_trajectory){
-    //                 printing_trajectory(&slice[0]); // try to do it every  1 second
-    //             }
+                // If printing trajectory is specified in parameters
+                if (analysis.print_trajectory){
+                    printing_trajectory(&slice[0]); // try to do it every  1 second
+                }
             
-    //         // If running Monte Carlo simulation is specified in parameters
-    //         } else if (sys.sim_type == MC_ALGORITHM) {
+            // If running Monte Carlo simulation is specified in parameters
+            } else if (sys.sim_type == MC_ALGORITHM) {
                 
-    //             // Run Monte Carlo cycle
-    //             mccycle(&slice[0]);
+                // Run Monte Carlo cycle
+                mccycle(&slice[0]);
             
-    //         // Otherwise, print error message
-    //         } else {
-    //             error("choose only MC = sys.sim_type==2 or bmd = sys.sim_type==0"); 
-    //         }
+            // Otherwise, print error message
+            } else {
+                error("choose only MC = sys.sim_type==2 or bmd = sys.sim_type==0"); 
+            }
 
-    //     }
+        }
 
-    //     // Terminate block; printing / measuring 
-    //     terminate_block(&slice[0]);
+        // Terminate block; printing / measuring 
+        terminate_block(&slice[0]);
 
-    //     // Print time for this block
-    //     time_t block2_1 = time(0);
-    //     time_t block2_0 ;
-    //     double datetime_diff2 = difftime(block2_1, block2_0);
-    //     printf("This block took %lf [sec]\n", datetime_diff2);
-    //      block2_0 =block2_1;
+        // Print time for this block
+        time_t block2_1 = time(0);
+        time_t block2_0 ;
+        double datetime_diff2 = difftime(block2_1, block2_0);
+        printf("This block took %lf [sec]\n", datetime_diff2);
+         block2_0 =block2_1;
 
-    // }
+    }
 
     // // Call function to print final statistics
-    // finalstat(&slice[0]);
+    finalstat(&slice[0]);
 
     // Print total time for program
     time_t t1 = time(0);
@@ -93,88 +93,166 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+void read_coordinates_from_file(Slice *current, int block_number){
+    // the file vi trajectroy.xyz  is read
+    // each block, separated by a white line (setWHITELINE=1), contains the folloing info in columns:
+    // 1: time, 2: particle number, 3-5: position, 6-9: quaternion
+    // which are of course NPART lines per block
+
+    static FILE *fp;
+    static int n_shots=0;
+    static double time_interval=1.;
+
+    int bufferLength=200; // maximum number of characters on line
+    char str[bufferLength];
+    int ipart,read_lines=0,WHITELINE=0;
+    static int linecount=0, init=1;
+
+    double time, x,y,z,time0;
+    vector r;
+    quaternion q;
+
+    char filename[600];
+	snprintf( filename, sizeof( filename ), "%s/trajectory.xyz", init_conf.directorypath);
+
+
+    // printf("read_coordinates_from_file .. block_number=%d \n",block_number);
+    if (init){
+    	// open only once in the beginning
+	    printf("read_coordinates_from_file %s\n",filename);
+    	fp = fopen(filename,"r");
+    	init=0;	
+    }
+
+    if(fp == NULL) {
+      error("Error opening file: trajectory.xyz");
+      return;
+    }
+
+    // walk over the file line by line
+    while(fgets(str, bufferLength, fp)!=NULL ) {
+        
+        if ((int)(linecount/(current->nparts+WHITELINE)) == block_number){
+            // read the block and save it to the slice
+            // printf(" reading snapshot number %d \n",block_number);
+
+            // printf("the string: %s",str);
+	        // dprint(linecount);
+	        // dprint((int)(linecount/(sys.npart )));
+	        // dprint(block_number);
+
+            sscanf(str,"%lf %d %lf %lf %lf %lf %lf %lf %lf\n",&time,&ipart,&r.x,&r.y,&r.z,&q.q0,&q.q1,&q.q2,&q.q3);
+            pbc(r,sys.boxl);
+            current->pts[ipart].r=r;
+            current->pts[ipart].q=normalize_quat(q);
+            // printf("read: %lf %d %lf %lf %lf %lf %lf %lf %lf\n",time,ipart,x,y,z,q.q0,q.q1,q.q2,q.q3);
+            // printf("read: %lf %lf %lf %lf\n",q0,q1,q2,q3);
+
+            //update the patch vectors;
+            update_patch_vector_ipart(current,ipart); 
+            
+            if (read_lines==0){
+	            // WE ASSUME THAT  the time_interval  between two frames is 1 second!!
+                current->c_time=block_number*time_interval;
+            }
+
+            //check for errors
+            if (read_lines!=ipart){
+                printf("the %dth particle that is read is not ipart=  %d at time=%.1lf \n",read_lines,ipart,current->c_time);
+                error("ERROR: there is a problem in read_coordinates_from_file.");     
+            }
+            
+            read_lines++;           
+        }
+        linecount++;
+        if (read_lines==sys.npart){
+            // printf("done reading block_number %d \n\n",block_number);
+            // reset_center(current); // do this if you want to put com at zero
+            // fclose(fp); // don't close the file just yet (its much faster if you only close it at the end)
+            n_shots+=1;
+            return;
+        }
+    }
+
+    printf("    you have reached the end of the trajectory file\n");
+    if (fgets(str, bufferLength, fp)==NULL){
+    	fclose(fp); // close the file now
+    	// free_all_memory();
+    	error(" end of file of trajectory.");
+    }
+    
+    return;
+}
+
+
 // // this is a very specific function .. 
-// void free_all_memory(void){
+void free_all_memory(void){
 
-//     printf("free_all_memory\n");
-//     free(&slice[0]);
-//     free(&start_slice[0]);
-//     freeBondTimeInfoArray(bondtimeinfoarray);
+    printf("free_all_memory\n");
 
-//     if (sys.sim_type==MC_ALGORITHM){
-//         free(&psl_old[0]);
-//     }
+    // do something with this 
 
-// }
+    // free(&slice[0]);
+    // free(&start_slice[0]);
+    // freeBondTimeInfoArray(bondtimeinfoarray);
 
-// void bmdcycle(Slice *psl) {
-//     /*sys.sim_type==0 Brownina Dynamics*/
-//     int istep, update=0, ipart;
-//     double dr2;
+    // if (sys.sim_type==MC_ALGORITHM){
+    //     free(&psl_old[0]);
+    // }
 
-//     // Perform overdamped langevin dynamics for langevin.step steps
-//     for(int istep=0;istep<langevin.step;istep++){
-//         propagate_bd(psl);        
-//     }
+}
 
-//     // If bond tracking is enabled, perform bond tracking for the current slice
-//     if(analysis.bond_tracking==1){
-//         bond_tracking(psl);
-//         // printBondTimeInfoArray(bondtimeinfoarray )
-//     }
 
-//     // Exit the function
-// }
 
-// void mc_warmup(Slice *psl){
-//  // Use mc_warmup to decorrelate from the input structure; perform MC moves without bond breakages
-//     printf("*********  mc_warmup is on:  equilibrating with MC ********\n");
+void mc_warmup(Slice *psl){
+ // Use mc_warmup to decorrelate from the input structure; perform MC moves without bond breakages
+    printf("*********  mc_warmup is on:  equilibrating with MC ********\n");
 
-//     // Save old settings to restore later
-//     double old_setting=analysis.bond_breakage;
-//     double old_beta=psl->beta;
-//     int old_clustermc=sys.cluster_MC;
-//     int nn=sys.nearest_neighbor;
+    // Save old settings to restore later
+    int old_setting=analysis.bond_breakage; // why was this a double
+    double old_beta=psl->beta;
+    int old_clustermc=sys.cluster_MC;
+    int nn=sys.nearest_neighbor;
 
-//     // Set new settings for MC warmup
-//     sys.nearest_neighbor=0; // For MC, set to neaghborlist to zero
-//     analysis.bond_breakage=0;
-//     psl->beta=1.; // Put temperature lower if you want to make stiffer bonds
-//     sys.cluster_MC=0;
-//     printf("There are %d bonds in the system, keep them fixed as analysis.bond_breakage= %lf.\n",psl->nbonds,analysis.bond_breakage);
+    // Set new settings for MC warmup
+    sys.nearest_neighbor=0; // For MC, set to neaghborlist to zero
+    analysis.bond_breakage=0;
+    psl->beta=1.; // Put temperature lower if you want to make stiffer bonds
+    sys.cluster_MC=0;
+    printf("There are %d bonds in the system, keep them fixed as analysis.bond_breakage= %d.\n",psl->nbonds,analysis.bond_breakage);
 
-//     // Perform MC warmup cycles
-//     for(int jcycle=0; jcycle<sys.mc_warmup; jcycle++) {
-//         for(int icycle=0; icycle<sys.ncycle2; icycle++) {
-//             mccycle(psl); // Perform MC cycle
-//         }             
+    // Perform MC warmup cycles
+    for(int jcycle=0; jcycle<init_conf.mc_warmup; jcycle++) {
+        for(int icycle=0; icycle<sys.ncycle2; icycle++) {
+            mccycle(psl); // Perform MC cycle
+        }             
 
-//         // Print status and optimize MC
-//         printstatusmc();
-//         optimizemc();
-//         printenergy_warmup(psl);
+        // Print status and optimize MC
+        printstatusmc();
+        optimizemc();
+        printenergy_warmup(psl);
 
-//         // Check for bond breakage
-//         if (psl->nbonds!=start_slice->nbonds){
-//             dprint(psl->nbonds);
-//             dprint(start_slice->nbonds);
-//             print_slice_information(psl);
-//             error("stop. breakage not allowed?");
-//         }
-//     }
+        // Check for bond breakage
+        if (psl->nbonds!=start_slice->nbonds){
+            dprint(psl->nbonds);
+            dprint(start_slice->nbonds);
+            print_slice_information(psl);
+            error("stop. breakage not allowed?");
+        }
+    }
 
-//     // Restore old settings
-//     analysis.bond_breakage=old_setting;
-//     psl->beta=old_beta; 
-//     sys.cluster_MC=old_clustermc;
-//     sys.nearest_neighbor=nn;
+    // Restore old settings
+    analysis.bond_breakage=old_setting;
+    psl->beta=old_beta; 
+    sys.cluster_MC=old_clustermc;
+    sys.nearest_neighbor=nn;
 
-//     // Update nearest neighbor list if necessary
-//     if (sys.nearest_neighbor)  update_nnlist(psl);
+    // Update nearest neighbor list if necessary
+    if (sys.nearest_neighbor)  update_nnlist(psl);
 
-//     // Exit the function
-//     return;
-// }
+    // Exit the function
+    return;
+}
 
 
 
@@ -183,8 +261,7 @@ void emptyfiles(){
     int status;
 
     DIR *dr = opendir(".");
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
-    { 
+    if (dr == NULL){  // opendir returns NULL if couldn't open directory 
         printf("Could not open current directory. No files are deleted. continue" ); 
         return ; 
     } 
