cf867960e39c33540a1bf45fdc489f7bd0e52ad6
hjjonas <h.j.jonas@uva.nl>
2024-02-20 12:06:30 +0100

    version_specific_functions.c version_specific_functions.h working on this file to add all the version_specific_functions it needs. including allocating and freeing memory

--- base_codes/terminate_block.h
+++ base_codes/terminate_block.h
@@ -7,6 +7,9 @@
 /*------------------GLOBAL VARIABLES-----------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
+#define MAX_FILENAME_LENGTH 100
+#define MAX_EXT_LENGTH 100
+#define MAX_VALUE_LENGTH 1000
 /*------------------STRUCTURE DEFINITIONS------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
@@ -16,14 +19,11 @@
 /*------------------GLOBAL FUNCTIONS-----------------------------------------*/
 extern void print_input(Slice *);
 extern void conf_output(Slice *);
-extern void printenergy(Slice *);
-extern void printenergy_warmup(Slice *);
+extern void printenergy(Slice *, char [MAX_EXT_LENGTH]);
 extern void print_slice_information(Slice *);
+extern void print_StatsLength_to_file(StatsLength *);
 extern void printing_trajectory(Slice *);
-extern void append_to_file(char [100], int , double );
-extern void print_to_file(char [100], int , double );
-extern void append_to_file2(char [100], char [1000] , char [100]  );
-extern void print_to_file2(char [100], char [1000] , char [100] );
+extern void write_append_to_file(char [100],  char [100], char ,char [1000] );
 extern void terminate_block(Slice *);
 extern void finalstat(Slice *);
 /*---------------------------------------------------------------------------*/
